16620f3696b00b6866b6bb7ee1821d1402234b04
Nick <NickHellemans93@gmail.com>
2020-03-25 00:52:28 +0100

    cypress wip

--- packages/admin-panel/cypress/support/index.js
+++ packages/admin-panel/cypress/support/index.js
@@ -16,5 +16,15 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
+
+Cypress.on('uncaught:exception', err => {
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  }
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
